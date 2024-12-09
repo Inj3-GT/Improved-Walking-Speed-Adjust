ad3d2608ef1b480d4d7244f6ce32230ed6983a5a
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2024-12-09 20:28:55 +0100

    v1.0

--- improved_walkingspeed_adjust/lua/ipr_walkingspeed_sys/configuration/ipr_walkingspeed_config.lua
+++ improved_walkingspeed_adjust/lua/ipr_walkingspeed_sys/configuration/ipr_walkingspeed_config.lua
@@ -2,8 +2,8 @@
 --- https://steamcommunity.com/id/Inj3/
 --- https://github.com/Inj3-GT
 --- GNU GENERAL PUBLIC LICENSE
-ipr_WalkSpeed_Config.ReduceSlowWalkSpeed = 0.75 --- Réduit la vitesse maximale de marche lente (mettre 1 si vous ne voulez pas de réduction de vitesse de marche lente). | Reduces maximum slow walk speed (set to 1 if you don't want slow walk speed reduction).
 ipr_WalkSpeed_Config.MaxRotation = 8 --- Le nombre de rotations avec la molette de votre souris pour atteindre la vitesse maximale (par défaut : 8). | The number of rotations with your mouse wheel to reach maximum speed (default: 8).
+ipr_WalkSpeed_Config.ReduceSlowWalkSpeed = 0.75 --- Réduit la vitesse maximale de marche lente (mettre 1 si vous ne voulez pas de réduction de vitesse de marche lente). | Reduces maximum slow walk speed (set to 1 if you don't want slow walk speed reduction).
 ipr_WalkSpeed_Config.AddKey = {true, --- Ajoute une touche supplémentaire et désactive automatiquement pour le sélecteur d'armes la molette de la souris lorsque vous appuyez sur la combinaison de touches KEY_CAPSLOCK + Molette (par défaut) ! | Adding an extra key and automatically disabled the weapon selector's mouse wheel when the KEY_CAPSLOCK + Wheel key combination is pressed (default) !
     key = KEY_CAPSLOCK --- Ajoute une touche supplémentaire KEY_CAPSLOCK (resté appuyé) combinée avec la molette de la souris (haut ou bas) pour activer la vitesse de marche dynamique. | Adds an additional button combined with the mouse wheel to activate dynamic walking speed.
 }
